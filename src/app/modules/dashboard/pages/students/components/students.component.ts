d45764e50b8c639da7baa36cefe6b50236685a3e
Federico Fernandez <fede-arq@hotmail.com>
2024-01-20 19:43:55 -0300

    se agrega sweetalerts a form y delete

--- src/app/modules/dashboard/pages/students/components/students.component.ts
+++ src/app/modules/dashboard/pages/students/components/students.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Student } from '../interface';
-
-
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -16,10 +15,37 @@ export class StudentsComponent {
 
   onStudentSubmit(ev: Student): void {
     this.dataSource = [...this.dataSource, {...ev, id: this.dataSource.length + 1}];
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "Your work has been saved",
+      showConfirmButton: false,
+      timer: 1500
+    });
+    
   }
 
   deleteStudent(student: Student): void {
-    this.dataSource = this.dataSource.filter(s => s.id !== student.id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.dataSource = this.dataSource.filter(s => s.id !== student.id);
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success"
+        });
+      }
+    });
+    
+    
   }
  
 }
