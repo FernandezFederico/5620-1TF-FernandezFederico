7b96b86fbbfeca295425d8649ea0dec0b49aa199
Federico Fernandez <fede-arq@hotmail.com>
2024-01-28 01:04:01 -0300

    se borran inputs sobrantes

--- src/app/modules/layout/components/sidenav/sidenav.component.ts
+++ src/app/modules/layout/components/sidenav/sidenav.component.ts
@@ -7,11 +7,6 @@ import { Component, Input } from '@angular/core';
   styleUrl: './sidenav.component.scss'
 })
 export class SidenavComponent {
-  @Input() isOpened: boolean = false;
   @Input() isSidenavOpen: boolean = false;
-
-  toggleSidenav() {
-    this.isOpened = !this.isOpened;
-  }
 
 }
